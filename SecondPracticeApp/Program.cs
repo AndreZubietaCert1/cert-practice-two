2d9dfa1a2dc746cc0d726f67a374fe5c1607fd6a
Andre Zubieta <andre.zubieta.cert1@gmail.com>
2024-05-05 22:33:54 -0400

    Updated Program to have a Singleton

--- SecondPracticeApp/Program.cs
+++ SecondPracticeApp/Program.cs
@@ -1,7 +1,10 @@
+using businesslogic.Managers;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddSingleton<PatientManager>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
