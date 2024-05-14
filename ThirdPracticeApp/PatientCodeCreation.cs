a9a113baeeafe8eb0eace3687487073145b721e9
Andre Zubieta <andre.zubieta.cert1@gmail.com>
2024-05-14 01:29:59 -0400

    removed example files

---
+++ ThirdPracticeApp/PatientCodeCreation.cs
@@ -0,0 +1,21 @@
+﻿namespace ThirdPracticeApp
+{
+    public class PatientCodeCreation
+    {
+        public string GeneratePatientCode(string firstName, string lastName, int ci)
+        {
+            // Extract first two letters of first name (or full first name if less than two letters)
+            string codePrefixA = firstName.Substring(0,1).ToUpper();
+            string codePrefixB = lastName.Substring(0, 1).ToUpper();
+
+            string codePrefix = codePrefixA + codePrefixB;
+
+            // Combine with hyphen and CI
+            string patientCode = $"{codePrefix}-{ci}";
+
+            return patientCode;
+        }
+
+
+    }
+}
